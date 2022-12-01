c3da732fb9feb0216dd6a8e68ee77be3311954dd
Natalia <nataxawolf@gmail.com>
2022-12-01 19:19:49 +0300

    homework #6, 8

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,20 @@
+﻿//Считываем данные с консоли
+string? number1L = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (number1L != null)
+{
+    //Преобразуем строковые данные в десятичные
+    int num1 = int.Parse(number1L);
+    //Находим остаток от деления
+    int rest = num1 % 2;
+    //Проверяем значение остатка и выводим результат
+    if (rest == 0)
+    {
+        Console.WriteLine("четное");
+    }
+    else
+    {
+        Console.WriteLine("нечетное");
+    }
+}
---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿//Считываем данные с консоли
+string? inputNumber = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (inputNumber != null)
+{
+    //Преобразуем строковые данные в десятичные
+    int inputNum = int.Parse(inputNumber);
+    //Задаем переменную для старта поиска четных чисел
+    int startNum = 2;
+    //Задаем строковую переменную для сбора результатов
+    string result = string.Empty;
+    //Заупскаем цикл-счетчик
+    while (startNum <= inputNum)
+    {
+        result = result + startNum + ", ";
+        startNum = startNum + 2;
+    }
+    Console.WriteLine(result);
+}
