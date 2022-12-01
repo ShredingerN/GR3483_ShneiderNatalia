037b6e551dd38f22e3009fd1c4beaa49bda95713
Natalia <nataxawolf@gmail.com>
2022-12-01 14:29:56 +0300

    homework #2,4

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Считываем данные с консоли
+string? number1L = Console.ReadLine();
+string? number2L = Console.ReadLine();
+
+//Проверяем, чтобы данные были не пустыми
+if (number1L != null && number2L != null)
+{
+    //Преобразуем строковые данные в десятичные
+    int num1 = int.Parse(number1L);
+    int num2 = int.Parse(number2L);
+    //Сравниваем два числа
+    if (num1 > num2)
+    {   
+        Console.WriteLine("max="+num1);
+    }
+    else
+    {
+        Console.WriteLine("max="+num2);
+    }
+
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿//Считываем данные с консоли
+string? number1L = Console.ReadLine();
+string? number2L = Console.ReadLine();
+string? number3L = Console.ReadLine();
+//Проверяем, чтобы данные были не пустыми
+if (number1L != null && number2L != null && number3L != null)
+{
+    //Преобразуем строковые данные в десятичные
+    int num1 = int.Parse(number1L);
+    int num2 = int.Parse(number2L);
+    int num3 = int.Parse(number3L);
+    // //Сравниваем поочередно 3 числа
+    // if (num1 > num2)
+    // {   
+    //     if (num1>num3)
+    //     {
+    //         Console.WriteLine("max="+num1);
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("max="+num3);
+    //     }
+    // }
+    // else
+    // {
+    //     if (num2>num3)
+    //     {
+    //         Console.WriteLine("max="+num2);
+    //     }    
+    //      else
+    //     {
+    //         Console.WriteLine("max="+num3);
+    //     }
+    // }
+    //использовала функцию Math.Max
+    int max = Math.Max(Math.Max(num1, num2), num3);
+    Console.WriteLine(max);
+
+}
