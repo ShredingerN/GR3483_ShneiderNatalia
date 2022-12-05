c0da2d527161700dd2d196dc47644ddc767df56a
Natalia <nataxawolf@gmail.com>
2022-12-05 16:53:14 +0300

    added homework

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -2,36 +2,15 @@
 
 //Ввод числа и проврка на null
 int inputNumber = int.Parse(Console.ReadLine() ?? "0");
-if (inputNumber > 99)
-{
-    int lengthNum = (int)Math.Log10(inputNumber) + 1;
-    int count = lengthNum;
-
-    while (count > 2)
-
-    {
-        int number = inputNumber / 10;
-        int result = number %10;
-    
-        Console.WriteLine(result);
-    }
-     count ++;
-   
-}
-else
-{
-    Console.WriteLine("в заданном числе нет 3-ей цифры");
-}
 
 //Вариант 1
-//Ввод числа и проврка на null
-//int inputNumber = int.Parse(Console.ReadLine() ?? "0");
+
 // //Проверяем число на трехзначность
 // if (inputNumber > 99)
 // {
 //     //Преобразуем введенное число в массив
 //     char[] digits = inputNumber.ToString().ToCharArray();
- 
+
 //     //выводим результат
 //     Console.WriteLine(digits[2]);
 // }
@@ -40,14 +19,23 @@ else
 //     Console.WriteLine("в заданном числе нет 3-ей цифры");
 // }
 
-//Варинат 2
+// Проверяем число на трехзначность.
+if (inputNumber > 99)
 
-//Console.WriteLine(result);
-// int lenNumber(int value)
-// {
-//     //int result = (int)Math.Log10(Math.Abs(value)) + 1;
-//     int result = (int)Math.Log10(value) + 1;
-//     if (value==0) result = 1;
-//     //return result;
-//     //return (value == 0) ? 1 : (int)Math.Log10(Math.Abs(value)) + 1;
-    
+{
+    //Определяем длину числа с помощью десятичного логарифма: количество цифр десятичной записи натурального числа равно 1 + [lg a].
+    int lengthNum = (int)Math.Log10(inputNumber) + 1;
+    //Находим трехзначное число путем деления на 10 в степени, равной "длина числа - 3".
+    int number = inputNumber / (int)Math.Pow(10, (lengthNum - 3));
+    //Находим остаток от деления трехзначного числа.
+    int result = number % 10;
+    //Выводим остаток, то есть 3-ю цифру заданного числа.
+    Console.WriteLine(result);
+
+}
+else
+{
+    Console.WriteLine("в заданном числе нет 3-ей цифры");
+}
+
+
