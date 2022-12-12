916bf292f7076560f03b04166e7945233408fb24
Natalia <nataxawolf@gmail.com>
2022-12-13 00:54:43 +0300

    sem4

---
+++ Sem4Task24/Program.cs
@@ -0,0 +1,53 @@
+﻿//Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А.
+
+//запрос данных у пользователя
+int ReadData(string line)
+{
+    //выводим сообщение
+    Console.Write(line);
+    //считываем число
+    int numberP = int.Parse(Console.ReadLine() ?? "0");
+    return numberP; //возвращаем значение
+}
+
+// Метод вывода результата
+void PrintResult(string line)
+{
+   Console.WriteLine(line);
+}
+
+//Суммируем от 1 до А, заказываем int (обязательно return того жн типа)
+int VariantSumSimple(int numA)
+{
+    int sumOfNumbers =0;
+    for(int i=1;i<=numA;i++)
+    {
+        //Присваивает левому операнду сумму левого и правого операндов: 
+        //выражение A += B равнозначно выражению A = A + B.
+        sumOfNumbers+=i;
+    }
+    return sumOfNumbers;
+}
+
+// Аналитический метод (формул Гаусса)
+int VariantSumGause(int numA)
+{
+    int sumOfNumbers = 0;
+    sumOfNumbers = ((1 + numA) * numA) / 2;
+    return sumOfNumbers;
+}
+
+int numberA = ReadData("Введите число А:");
+
+DateTime d1 = DateTime.Now; //подсчет времени выполнения программы
+//в результат кладем метод и переменную исходную.
+int res1 = VariantSumSimple(numberA);
+//Вывод результата времени выполнения
+Console.WriteLine(DateTime.Now - d1);
+
+DateTime d2 = DateTime.Now;
+int res2 = VariantSumGause(numberA);
+Console.WriteLine(DateTime.Now - d2);
+
+PrintResult("Сумма чисел от 1 до A равна(простой метод):" + res1 );
+PrintResult("Сумма чисел от 1 до A равна(простой Гауса):" + res2 );
---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+//1. Метод для встречи данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//2. Методы вывода массива, с вложенным методом. 
+void PrintData(string res, int[] arr)
+{
+    Console.WriteLine(res);
+    PrintArr(arr);
+}
+
+int[] GenArr(int num)
+{
+    Random rnd = new Random();
+    int[] arr = new int[num];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+//3. Просим ввести длину массива
+int arrLen = ReadData("Введите длину массива.");
+//4.Задаем переменную для массива, кладем метод для генерации массива
+int[] arr = GenArr(arrLen);
+//5. Печатем готовый массив
+PrintData("Сгененрированный массив:",arr);
+
