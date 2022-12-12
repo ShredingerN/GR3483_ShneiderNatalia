b26e97e862f667261afcc137f5b9f84aa5ab7088
Natalia <nataxawolf@gmail.com>
2022-12-12 19:23:11 +0300

    home work

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,68 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//Вариант 1
+
+//---------------------------------------------------------------------------------------------------------------------
+// Этот вариант мне не очень нравиться, если вводится не 5-значное число, дополнительно к коментарию выскакивает false:
+// Это не пятизначное число
+// Ответ: False
+//---------------------------------------------------------------------------------------------------------------------
+int number = ReadData("Введите число : ");
+
+PrintData("Ответ: ", PalinTest(number));
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+bool PalinTest(int num)
+{
+    bool result = false;
+    if (number > 9999 && number < 100000)
+    {
+        if ((num / 10000 == num % 10) && ((num / 1000) % 10 == ((num / 10) % 10))) result = true;
+
+    }
+    else Console.WriteLine("Это не пятизначное число");
+    return result;
+}
+
+void PrintData(string msg, bool res)
+{
+    Console.WriteLine(msg + res);
+}
+
+//Вариант 2
+
+//----------------------------------------------------------------
+// Здесь возможно код не очень красивый, но нет лишнего в выводе
+//----------------------------------------------------------------
+
+// int number = ReadData("Введите число : ");
+
+// if (number > 9999 && number < 100000)
+// {
+//     PrintData("Ответ: ", PalinTest(number));
+// }
+// else
+// {
+//     Console.WriteLine("Это не пятизначное число");
+// }
+
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return int.Parse(Console.ReadLine() ?? "0");
+// }
+
+// bool PalinTest(int num)
+// {   
+//     bool result = ((num / 10000 == num % 10) && ((num / 1000) % 10 == ((num / 10) % 10)));
+//     return result;
+// }
+
+// void PrintData(string msg, bool res)
+// {
+//     Console.WriteLine(msg + res);
+// }
---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int coorx1 = ReadData("Введите координату x1");
+int coorx2 = ReadData("Введите координату x2");
+int coory1 = ReadData("Введите координату y1");
+int coory2 = ReadData("Введите координату y2");
+int coorz1 = ReadData("Введите координату z1");
+int coorz2 = ReadData("Введите координату z2");
+
+PrintData("Расстояние между точками: ", CountLenпth(coorx1, coory1, coorx2, coory2, coorz1, coorz2));
+
+//Метода читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+//Метод находит расстояние между точками на плоскости
+double CountLenпth(double X1, double Y1, double X2, double Y2, double Z1, double Z2)
+{
+    double result = Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2) + Math.Pow(Z1 - Z2, 2));
+    return result;
+}
+
+//Метод выводит данные пользователю
+void PrintData(string msg, double res)
+{
+    Console.WriteLine(msg + res);
+}
+
