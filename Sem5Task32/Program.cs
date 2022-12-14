57f7683a81fa34cec626982e03e57c5caf15f642
Natalia <nataxawolf@gmail.com>
2022-12-15 01:35:16 +0300

    sem 5

---
+++ Sem5Task32/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+int[] array = GenArr(10, -9, 9);
+Print1DArr(array);
+int[] newrray = InverseArr(array);
+Print1DArr(newrray);
+
+int[] GenArr(int len, int minVAlue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(minVAlue, maxValue + 1);
+    }
+    return arr;
+}
+
+int[] InverseArr(int[]arr)
+{
+    int[]outArr = new int[arr.Length];
+    for(int i=0; i<arr.Length;i++)
+    {
+        outArr[i]=arr[i]*(-1);
+    }
+    return outArr;
+}
+
+   void Print1DArr(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            Console.Write(arr[i] + ", ");
+        }
+        Console.WriteLine(arr[arr.Length - 1]);
+    }
---
+++ Sem5Task33/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+int[] array = GenArray(10, -9, 10);
+Print1DArr(array);
+int num = ReadData("Введите число");
+int result = SearchElem(array, num);
+
+if (result > 0)
+{
+    PrintData("Элемент найден в позиции" + result);
+}
+else
+{
+    PrintData("Элемент не найден");
+}
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int[] GenArray(int len, int minVAlue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(minVAlue, maxValue + 1);
+    }
+    return arr;
+}
+
+void Print1DArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1]);
+}
+// Ищем элемент
+int SearchElem(int[] arr, int elem)
+{
+    int res = -1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] == elem)
+        {
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
+
