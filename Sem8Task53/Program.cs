dbad9796ac3cee7f9ff769e4a4d288fb1635bb31
Natalia <nataxawolf@gmail.com>
2022-12-25 18:29:56 +0300

    sem 8

---
+++ Sem8Task53/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+int m = ReadData("Введите количество строк M: ");
+int n = ReadData("Введите количество столбцов N: ");
+int[,] matrix = Fill2DArray(m, n, 1, 9);
+Print2DArray(matrix);
+Change2DArray(matrix);
+Print2DArray(matrix);
+
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+// Печать двумерного массива
+void Print2DArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void Change2DArray(int[,] matr)
+{
+    // идем по строкам, меняются столбцы
+    int temp = 0;
+    for (int i = 0; i < matr.GetLength(1); i++)
+    {
+        temp = matr[0, i];
+        matr[0, i] = matr[matr.GetLength(0) - 1, i];
+        matr[matr.GetLength(0) - 1, i] = temp;
+    }
+}
